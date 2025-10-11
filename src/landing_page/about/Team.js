cf83024e91b1c2a57867a6d2a1411e0b82bc1e94
student-shayanalam <smshayanalam@gmail.com>
2025-10-11 13:02:01 +0530

    Update Team component: replace Nithin Kamath's details with Shayan Alam's profile and image

--- src/landing_page/about/Team.js
+++ src/landing_page/about/Team.js
@@ -13,28 +13,46 @@ function Team() {
       >
         <div className="col-12 col-md-6 p-3 p-md-4 text-center">
           <img
-            src="media/images/nithinKamath.jpg"
-            alt="Nithin Kamath"
+            src="media/images/shayanAlam.jpeg"
+            alt="Shayan Alam"
             className="img-fluid"
             style={{ borderRadius: "100%", width: "55%", maxWidth: "250px" }}
           />
-          <h4 className="mt-3 fs-5 fs-md-4">Nithin Kamath</h4>
-          <h6 className="fs-6 fs-md-5">Founder & CEO</h6>
+          <h4 className="mt-3 fs-5 fs-md-4">Shayan Alam</h4>
+          <h6 className="fs-6 fs-md-5">Frontend Developer</h6>
         </div>
+
         <div className="col-12 col-md-6 p-3 p-md-4" style={{ fontSize: "0.9em" }}>
           <p>
-            Nithin bootstrapped and founded Zerodha in 2010 to overcome the
-            hurdles he faced during his decade long stint as a trader. Today,
-            Zerodha has changed the landscape of the Indian broking industry.
+            Shayan is a Frontend Developer from Korba, Chhattisgarh, currently
+            working and learning in a small IT company. He has mastered the basics
+            of HTML, CSS, JavaScript, React.js, and Git/GitHub, and is now
+            exploring Node.js, Express, and the MERN stack.
           </p>
+
           <p>
-            He is a member of the SEBI Secondary Market Advisory Committee
-            (SMAC) and the Market Data Advisory Committee (MDAC).
+            With a strong foundation in mathematics and a problem-solving mindset,
+            Shayan has built and deployed multiple learning projects including a
+            Weather App, a Zerodha clone, a Zoom-like WebRTC project, and a
+            ChatGPT integration using OpenAI APIs.
           </p>
-          <p>Playing basketball is his zen.</p>
+
           <p>
-            Connect on <a href="">Homepage</a> / <a href="">TradingQnA</a> /{" "}
-            <a href="/twitter">Twitter</a>
+            He is also developing a School Management System to automate the
+            workflow of his family’s school, NEW STAR ENGLISH SCHOOL, using
+            TypeScript, Node.js, and Express. His goal is to bring digital
+            solutions to education in small towns like Korba.
+          </p>
+
+          <p>
+            When not coding, Shayan enjoys teaching mathematics and exploring new
+            ideas to make school management easier through automation.
+          </p>
+
+          <p>
+            Connect on{" "}
+            <a href="#">Homepage</a> / <a href="#">LinkedIn</a> /{" "}
+            <a href="#">GitHub</a>
           </p>
         </div>
       </div>
